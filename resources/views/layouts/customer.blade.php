29a1b1cd78adf22ff71cc45efaab6592e3a8c978
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-11-07 14:45:50 +0200

    Added subscription

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.svg" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.svg" />
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
