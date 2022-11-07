1e62ae93fe5939cdcd9e324ec34b2820092c027d
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-11-07 15:10:11 +0200

    favicons

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -7,7 +7,10 @@
     <meta name="description" content="@yield('description')" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.svg') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     <title>Igihozo couture @yield('title')</title>
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
@@ -44,7 +47,7 @@
     <a href="#" class="scroll-top">
         <i class="lni lni-chevron-up"></i>
     </a>
-    <x-footer-component/>
+    <x-footer-component />
 
     <!-- ========================= JS here ========================= -->
     <!-- JavaScript Bundle with Popper -->
