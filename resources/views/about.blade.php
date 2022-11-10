b9e4a213c86bb1826ecbde0a8659c31d66d1a5ec
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-11-10 13:10:26 +0200

    test image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -24,16 +24,19 @@
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12 col-12">
                     <div class="content-left">
-                        <img src="{{ asset('images/logo/logo.png') }}" alt="#">
-                        <a href="https://www.youtube.com/shorts/tlHCnvk96eM">
-                            class="glightbox video"><i class="lni lni-play"></i></a>
+                        <iframe width="420" height="676" src="https://www.youtube.com/embed/tlHCnvk96eM?autoplay=1&mute=1" title=""
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscren>                                                                                                                                                                                                                                                                                                                                                                        </iframe>
+
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12 col-12">
 
                     <div class="content-right">
                         <h2>{{ config('app.name') }} - Your Trusted & Reliable Fashion Designer.</h2>
-                        <p>Igihozo La maison de Couture is a social enterprise founded by two women and one man, and is into
+                        <p>Igihozo La maison de Couture is a social enterprise founded by two women and one man, and is
+                            into
                             tailoring and garment business.
                             To face and solve the problems of clothing industry, the efficiency of each link in apparel
                             production needs to be enhanced.
@@ -48,7 +51,8 @@
                         <h2>Vision</h2>
                         <p>The enterprise was founded by two women and one man with a vision to positively impacting the
                             society by supporting homeless girls through giving them opportunities to training, fighting
-                            against poverty and in the process protecting them from sexual abuse and unplanned pregnancies
+                            against poverty and in the process protecting them from sexual abuse and unplanned
+                            pregnancies
                             due to poverty as well as protecting their mental health.</p>
                     </div>
                 </div>
@@ -56,7 +60,8 @@
                     <div class="content-right">
                         <h2>Goals</h2>
                         <p>The enterprise goal is to have a factory big enough to take in many young girls for capacity
-                            building and to increase the productivity of made in Rwanda clothes since second hand clothes
+                            building and to increase the productivity of made in Rwanda clothes since second hand
+                            clothes
                             are not allowed on the Rwandan market anymore.</p>
                     </div>
                 </div>
