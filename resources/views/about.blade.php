74c7a62507843cd5fdde4ac605a2416595c764c2
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-11-10 10:55:34 +0200

    added content on about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -26,139 +26,40 @@
                     <div class="content-left">
                         <img src="assets/images/about/about-img.jpg" alt="#">
                         <a href="https://www.youtube.com/watch?v=r44RKWyfcFw&fbclid=IwAR21beSJORalzmzokxDRcGfkZA1AtRTE__l5N4r09HcGS5Y6vOluyouM9EM"
-                           class="glightbox video"><i class="lni lni-play"></i></a>
+                            class="glightbox video"><i class="lni lni-play"></i></a>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12 col-12">
 
                     <div class="content-right">
-                        <h2>ShopGrids - Your Trusted & Reliable Partner.</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus
-                            pellentesque
-                            faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo.
-                            Morbi
-                            vitae est eget dolor consequat aliquam
-                            eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat
-                            lectus,
-                            ultrices euismod sagittis.
+                        <h2>{{ config('app.name') }} - Your Trusted & Reliable Fashion Designer.</h2>
+                        <p>Igihozo La maison de Couture is a social enterprise founded by two women and one man, and is into
+                            tailoring and garment business.
+                            To face and solve the problems of clothing industry, the efficiency of each link in apparel
+                            production needs to be enhanced.
                         </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius mod tempor incididunt
-                            ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi aliquip ex ea commodo consequat.</p>
+
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-
-
-    <section class="team section">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-title">
-                        <h2 class="wow fadeInUp" data-wow-delay=".4s">Our Core Team</h2>
-                        <p class="wow fadeInUp" data-wow-delay=".6s">There are many variations of passages of Lorem
-                            Ipsum
-                            available, but the majority have suffered alteration in some form.</p>
+            <div class="row align-items-center py-4 ml-2">
+                <div class="col-lg-6 col-md-12 col-12 ml-2 mr-2">
+                    <div class=" content-left">
+                        <h2>Vision</h2>
+                        <p>The enterprise was founded by two women and one man with a vision to positively impacting the
+                            society by supporting homeless girls through giving them opportunities to training, fighting
+                            against poverty and in the process protecting them from sexual abuse and unplanned pregnancies
+                            due to poverty as well as protecting their mental health.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-12">
+                    <div class="content-right">
+                        <h2>Goals</h2>
+                        <p>The enterprise goal is to have a factory big enough to take in many young girls for capacity
+                            building and to increase the productivity of made in Rwanda clothes since second hand clothes
+                            are not allowed on the Rwandan market anymore.</p>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-12">
-
-                    <div class="single-team">
-                        <div class="image">
-                            <img src="assets/images/team/01.jpg" alt="#">
-                        </div>
-                        <div class="content">
-                            <div class="info">
-                                <h3>Grace Wright</h3>
-                                <h5>Founder, CEO</h5>
-                                <ul class="social">
-                                    <li><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-skype"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-lg-3 col-md-6 col-12">
-
-                    <div class="single-team">
-                        <div class="image">
-                            <img src="assets/images/team/02.jpg" alt="#">
-                        </div>
-                        <div class="content">
-                            <div class="info">
-                                <h3>Taylor Jackson</h3>
-                                <h5>Financial Director</h5>
-                                <ul class="social">
-                                    <li><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-skype"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-lg-3 col-md-6 col-12">
-
-                    <div class="single-team">
-                        <div class="image">
-                            <img src="assets/images/team/03.jpg" alt="#">
-                        </div>
-                        <div class="content">
-                            <div class="info">
-                                <h3>Quinton Cross</h3>
-                                <h5>Marketing Director</h5>
-                                <ul class="social">
-                                    <li><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-skype"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-lg-3 col-md-6 col-12">
-
-                    <div class="single-team">
-                        <div class="image">
-                            <img src="assets/images/team/04.jpg" alt="#">
-                        </div>
-                        <div class="content">
-                            <div class="info">
-                                <h3>Liana Mullen</h3>
-                                <h5>Lead Designer</h5>
-                                <ul class="social">
-                                    <li><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
-                                    </li>
-                                    <li><a href="javascript:void(0)"><i class="lni lni-skype"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
     </section>
 @endsection
